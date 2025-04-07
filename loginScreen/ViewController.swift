b2fad44da3e034a1547fcf9d1fec8a0c88f22dd2
Daniil Isakov <serverpgig@yandex.ru>
2025-04-07 17:03:05 +0300

    add IBO

--- loginScreen/ViewController.swift
+++ loginScreen/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var saveUsernameSwitch: UISwitch!
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
