bc4cf5140ac7a7a85d8ce334840e815de58e54ba
Daniil Isakov <serverpgig@yandex.ru>
2025-04-07 17:34:02 +0300

    Implement login logic with username and password saving

--- loginScreen/ViewController.swift
+++ loginScreen/ViewController.swift
@@ -12,13 +12,47 @@ class ViewController: UIViewController {
     @IBOutlet var saveUsernameSwitch: UISwitch!
     @IBOutlet var usernameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        passwordTextField.isSecureTextEntry = true
+        if let savedUsername = UserDefaults.standard.string(forKey: "username") {
+            usernameTextField.text = savedUsername
+        }
+        
+        if let savedPassword = UserDefaults.standard.string(forKey: "password") {
+            passwordTextField.text = savedPassword
+        }
+        
+        let isSwitchOn = UserDefaults.standard.bool(forKey: "saveUsernameSwitchState")
+        saveUsernameSwitch.isOn = isSwitchOn
     }
 
     @IBAction func loginButtonTapped(_ sender: UIButton) {
+        let username = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        if username.isEmpty || password.isEmpty {
+            print("Please enter both username and password.")
+            return
+        }
+        
+        if saveUsernameSwitch.isOn {
+            UserDefaults.standard.set(username, forKey: "username")
+            UserDefaults.standard.set(password, forKey: "password")
+        } else {
+            UserDefaults.standard.removeObject(forKey: "username")
+            UserDefaults.standard.removeObject(forKey: "password")
+        }
+        
+        UserDefaults.standard.set(saveUsernameSwitch.isOn, forKey: "saveUsernameSwitchState")
+
+        print("Login successful")
     }
+    
+    
+    
+    
     
 }
 
