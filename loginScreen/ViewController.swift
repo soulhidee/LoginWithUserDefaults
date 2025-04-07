67f656d464a3feb1dc4a2d4489e8817dd7bcf6f8
Daniil Isakov <serverpgig@yandex.ru>
2025-04-07 16:53:47 +0300

    start

---
+++ loginScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginScreen
+//
+//  Created by Даниил on 07.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
